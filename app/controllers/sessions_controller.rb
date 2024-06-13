1f387e6ceeef90526be5713f2d9b96636ac0f045
adammiwa88 <adammiwa88@gmail.com>
2024-06-12 21:02:35 -0400

    rest of chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' #Not quite right!
-render 'new', status: :unprocessable_entity
+      render 'new', status: :unprocessable_entity
+    end
   end
-end
 
   def destroy
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
