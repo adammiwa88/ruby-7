c8d30f932a963982a4933d52e766d6c578f49a81
adammiwa88 <adammiwa88@gmail.com>
2024-06-24 10:15:26 -0400

    chapter 9 changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,17 +5,33 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    class TestCase
+      # Run tests in parallel with specified workers
+      parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-  include ApplicationHelper
+      # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+      fixtures :all
+    include ApplicationHelper
 
-  def is_logged_in?
-    !session[:user_id].nil?
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+
+    def log_in_as(user)
+  session[:user_id] = user.id
+    end
   end
+
+end
+
+  class ActionDispatch::IntegrationTest
+
+    # Log in as a particular user.
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+  
 
     # Add more helper methods to be used by all tests here...
   end
